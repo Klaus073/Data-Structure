46f8e7c31a677b5fe03e11b0b67c111afdb3a11a
Nauman Rao <65257448+Klaus073@users.noreply.github.com>
2021-01-26 10:36:07 +0500

    Add files via upload

---
+++ cbt_insertion_ass.cpp
@@ -0,0 +1,157 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+struct node
+{
+    int val;
+    node *left;
+    node *right;
+};
+class CBT
+{
+private:
+    node *root;
+public:
+    CBT(){      //constructor
+        node *left=NULL;
+        node *right=NULL;
+        root=NULL;
+    }
+    
+    node* Get_Root() 
+    {
+		return root;
+	}
+
+int size(node* node)  
+    {  
+    if (node == NULL)  
+       return 0;  
+    else
+      return(1+size(node->left) + size(node->right));  
+    }
+
+int maxDepth(node* temp)  
+{  
+    if (temp == NULL)  
+        return 0;  
+    else
+    {  
+        /* compute the depth of each subtree */
+
+        int lDepth = maxDepth(temp->left);  
+        int rDepth = maxDepth(temp->right);  
+      
+        /* use the larger one */
+        if (lDepth > rDepth)
+        {
+        
+        
+            return(lDepth + 1);  
+        }
+        else
+        {
+         return(rDepth + 1);
+        }  
+    }  
+} 
+
+node* Create(int data) {
+		node *Node = new node();
+		Node->val = data;
+		Node->left == NULL;
+		Node->right == NULL;
+		return Node;
+	}
+
+void CBTinsertion(node *temp,int data)
+{
+   
+    if (root == NULL)
+        {
+			root = Create(data);
+			return;
+		}
+        int height = maxDepth(temp->left);  // it will pass the node to height function and ll calculate height
+            int expected_node = pow(2, height) - 1; // 
+            int left_tree_node_count = size(temp->left);
+        int right_tree_node_count = size(temp->right);
+   
+    if(left_tree_node_count==right_tree_node_count)
+    {
+        if (temp->left==NULL)
+        {
+             temp->left = Create(data);
+             return;
+        }
+        
+           else  CBTinsertion(temp->left,data);
+        }
+    else if(expected_node != left_tree_node_count)
+        {
+            if(temp->left == NULL)
+            {
+               // cout<<"temp left = null"<<endl;
+                temp->left = Create(data);
+              
+                return;
+            }
+            else 
+            {
+                
+                CBTinsertion(temp->left,data);
+            }
+        }
+    else if(temp->right == NULL)
+        {
+           
+            temp->right = Create(data);
+            //root=temp;
+            return;
+        }
+        
+            else
+            {
+                
+                CBTinsertion(temp->right,data);
+            }
+            
+        
+        
+        
+}
+ void print(node *temp) {
+		if (temp == NULL)
+			return;
+
+         cout << temp->val << " ";   
+		print(temp->left);
+		
+		print(temp->right);
+	}
+    
+};
+
+
+
+int main()
+{
+    CBT cbt;
+   
+
+    cbt.CBTinsertion(cbt.Get_Root(), 10);
+    cbt.CBTinsertion(cbt.Get_Root(), 11);
+     cbt.CBTinsertion(cbt.Get_Root(), 16);
+    cbt.CBTinsertion(cbt.Get_Root(), 5);
+     cbt.CBTinsertion(cbt.Get_Root(), 23);
+    cbt.CBTinsertion(cbt.Get_Root(), 17);
+     cbt.CBTinsertion(cbt.Get_Root(), 15);
+    cbt.CBTinsertion(cbt.Get_Root(), 33);
+
+    
+    cbt.print(cbt.Get_Root());
+   
+    return 0;
+}
+   
